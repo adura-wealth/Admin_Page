d7eedfb9c8a94cab414474361788e35657ceab32
adura-wealth <olayiwolaaduragbemi3@gmail.com>
2025-07-22 13:04:22 +0100

    updated

--- berserk/views.py
+++ berserk/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def disc(request):
     return HttpResponse("hello world")
+
+def contact(request):
+    return HttpResponse("contact")
